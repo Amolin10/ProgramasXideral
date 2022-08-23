b72e6c5b3b87c32ed1ad484bf0c976054bfd0064
Amolin10 <molina102997@gmail.com>
2022-08-23 10:19:10 -0500

    se implemento lombok

--- restClient/src/main/java/com/client/rest/model/Deportista.java
+++ restClient/src/main/java/com/client/rest/model/Deportista.java
@@ -1,78 +1,19 @@
 package com.client.rest.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-/**
- * Clase deportista, tiene los mismos atributos que la tabla deportista en la base de datos
- * @author Amolin
- */
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+@ToString
 public class Deportista {
-
-	// define fields
+	
 	private int id;
 	private String nombre;
 	private String apellido;
 	private String deporte;
 	
-	public Deportista() {
-		
-	}
-
-	/**
-	 * Constructor de la clase
-	 * @param nombre
-	 * @param apellido
-	 * @param deporte
-	 */
-	public Deportista(String nombre, String apellido, String deporte) {
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.deporte = deporte;
-	}
-	
-	public Deportista(int id, String nombre, String apellido, String deporte) {
-		this.id = id;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.deporte = deporte;
-	}
-
-	// define getter/setter
-	
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-
-	public String getDeporte() {
-		return deporte;
-	}
-
-	public void setDeporte(String deporte) {
-		this.deporte = deporte;
-	}
-
-	@Override
-	public String toString() {
-		return "Deportista [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", deporte=" + deporte + "]";
-	}
-		
 }
